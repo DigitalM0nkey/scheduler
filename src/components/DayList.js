aa995808de2ba106dea4571f38dd425ab460448e
Terrence Kuiper <github@terrencekuiper.com>
2019-10-28 16:50:26 -0700

    implement useState

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,11 +6,11 @@ export default function DayList(props) {
     return (
       <ul>
         <DayListItem
-          id={day.id}
           name={day.name}
           spots={day.spots}
           selected={day.name === props.day}
           setDay={props.setDay}
+          key={day.id}
         />
       </ul>
     );
