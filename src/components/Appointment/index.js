47e98beadc4e5fe6b92194fe61449dbef34ae1b2
Terrence Kuiper <github@terrencekuiper.com>
2019-10-29 22:44:01 -0700

    Update index to include header

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <article className="appointment">
+      <header>
+        {props.time}
+        {props.interview ? <Show /> : <Empty />}
+      </header>
+    </article>
+  );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -18,6 +18,7 @@ import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
 import Form from "components/Appointment/Form";
+import index from "components/Appointment/index";
 
 storiesOf("Button", module)
   .addParameters({
@@ -180,4 +181,10 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
   ));
