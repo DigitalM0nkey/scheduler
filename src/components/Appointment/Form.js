e73864a920903bbe7be62412643ac4fb0c6bcdef
Terrence Kuiper <github@terrencekuiper.com>
2019-10-29 20:56:58 -0700

    Update Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,16 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,6 +23,7 @@ export default function Form(props) {
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
+            onChange={event => setName(event.target.value)}
             placeholder="Enter Student Name"
             /*
           This must be a controlled component
@@ -27,7 +38,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>
+          <Button danger onClick={cancel}>
             Cancel
           </Button>
           <Button confirm onClick={props.onSave}>
