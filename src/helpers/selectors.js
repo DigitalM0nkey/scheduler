ae7790a18c5733a239548f84572336f8762a3523
Terrence Kuiper <github@terrencekuiper.com>
2019-10-30 20:48:09 -0700

    Create helper function and tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+  let index = state.days.findIndex(x => x.name === day);
+  let appointmentKeys = [];
+  let appointments = [];
+  if (state.days.length === 0) {
+    return [];
+  }
+  if (index >= 0) {
+    appointmentKeys = state.days[index].appointments;
+  } else {
+    return [];
+  }
+  appointmentKeys.forEach(element => {
+    appointments.push(state.appointments[element]);
+  });
+
+  return appointments;
+}
